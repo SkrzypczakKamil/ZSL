332243a2e952ef5167bc248404d9b4dd605e2d98
HuntClauss <39533833+HuntClauss@users.noreply.github.com>
2022-03-14 08:24:07 +0100

    Update index.php

--- funkcje/index.php
+++ funkcje/index.php
@@ -7,12 +7,10 @@
   <body>
     <h3> Dane użytkownika</h3>
     <form action="./scripts/script.php" method="post">
-      <input type="text" name="name" placeholder="Podaj imię"> <br><br>
-      <input type="color" name="color"> Wybierz Kolor <br><br>
-      <input type="text" name="nationality" placeholder="Narodowosc"><br><br>
+      <input type="text" name="name" placeholder="Podaj imię">
+      <input type="color" name="color">Wybierz Kolor
+      <input type="text" name="nationality" placeholder="Narodowosc">
       <input type="submit" value="zatwierdź">
-
-
   </form>
   </body>
 </html>
